21f82a95741097e6d25cb6a58bbd561d4e178e39
CPT-CoLd <61501142+CPT-CoLd@users.noreply.github.com>
2020-02-28 12:12:35 +0100

    Add files via upload

---
+++ Dbot.lua
@@ -0,0 +1,15 @@
+print("welcome I am Dbot i can do some things be Patinet with me")
+
+while input ~= "exit" do
+     input = io.read()
+     if input == "hello" then
+         print("hello nice to meet you")
+     elseif input == "bye" then
+         print("ok, bye see you later")
+     elseif input == "Ora Ora Ora" then
+         print("ZA WARUDO, muda muda muda muda muda muda muda")
+     elseif input == fuck you
+     else
+         print("Sorry this is no valid Question")
+     end
+end
